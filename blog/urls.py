e35a1a0630cbc8006eaa2e9dbb51cda9bcec5b46
Ansah-98 <yungtupac99@gmail.com>
2023-01-08 12:44:12 +0000

    styling

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import post_list,post_detail
+from blog.views import post_list,post_detail,PostListView
 
 urlpatterns  = [
     path('',post_list, name = 'post_l'),
-    path('<int:pk>',post_detail,name = 'post_d')]
+    path('<int:pk>',post_detail,name = 'post_d'),
+    path('post', PostListView.as_view(),name ='postclass')]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
+from django.views.generic import ListView
 from django.shortcuts import render
-
-# Create your views here.
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from blog.models import Post
+# Create your views here.
 
 def post_list(request):
     posts  = Post.objects.all()
-    return render(request,'blog/list.html',{'posts':posts})
+    # paginator = Paginator(posts,3)
+    # page = request.GET.get('page')
+    # try:
+    #     pages  = paginator.page(page)
+
+    # except PageNotAnInteger:
+    #     pages = paginator.page(1)
+    # except EmptyPage:
+    #     pages = paginator.page(paginator.num_pages)
+
+    return render(request,'blog/list.html',{'posts':posts })
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    template_name ='blog/list.html'
 
 def post_detail(request,id):
     post = Post.objects.get(id = id)
